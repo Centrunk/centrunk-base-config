6c0c8604244661631f04326e09b5404543108dcd
Steven Wilson <tehhiper1@gmail.com>
2022-08-22 12:10:32 -0500

    Adding CCM S3 upload

--- bin/ccm.sh
+++ bin/ccm.sh
@@ -6,6 +6,7 @@ inotifywait -m /opt/dvm/config.centrunk.yml -e create -e modify |
         echo "The file '$file' appeared in directory '$dir' via '$action'"
         # do something with the file
         # use awscli to upload file to minio
+        aws --endpoint-url https://10.147.17.114:9000 s3 cp /opt/dvm/config.centrunk.yml s3://ctrs-ccm/$(zerotier-cli ip4)/config.centrunk.yml
     done
 
 # 3.0 TODO, handle remote updates, compare hashes every so often
